6f1267cd5e169dab692851ea4adc594a539d0a38
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-27 21:45:38 +0530

    for extracting the extension

--- extension.py
+++ extension.py
@@ -6,9 +6,8 @@ for root,dirs,files in os.walk('/root/devops_task3'):
         if file.endswith('.php'):
           os.system("kubectl create -f /root/devops_task3/jenkins_pvc.yml")
           os.system("kubectl create -f /root/devops_task3/deploy.yml")
-          os.system("kubectl expose deploy myweb-deploy --type=NodePort --port=80")
         elif file.endswith('.html'):
           os.system("kubectl create -f /root/devops_task3/jenkins1_pvc.yml")
           os.system("kubectl create -f /root/devops_talk3/deploy1.yml")
-          os.system("kubectl expose deploy myweb-deploy1 --type=NodePort --port=80")
+          
             
