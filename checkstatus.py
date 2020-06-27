18957fbc5ceceb46f39b5783ad8f9b686e2e6f40
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-27 19:57:57 +0530

    For checking the status

--- checkstatus.py
+++ checkstatus.py
@@ -1,7 +1,7 @@
-import susprocess as sp
-if(sp.getoutput("curl -o /dev/null -s -w \"%{http_code}\n\" php_code") == "200"): 
+import os
+if(os.system("$(curl -o /dev/null -s -w \"%{http_code}\" 192.168.99.108:31000/*.php)") == "200"): 
     pass
-elif(sp.getoutput("curl -o /dev/null -s -w \"%{http_code}\n\" html_code")== "200"):
+elif(os.system("$(curl -o /dev/null -s -w \"%{http_code}\" 192.168.99.108:31000/*.html)") == "200"):
     pass
 else:
     with open("/status" , "w") as f:
