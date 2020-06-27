3c4000d414b99e03b415180f68b9c27a5f9f3144
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-27 17:22:21 +0530

    check the extension of the coding file

--- extension.py
+++ extension.py
@@ -1,10 +1,11 @@
 import os
+os.system("kubectl create -f jenkins_pvc.yml")
 os.system("kubectl create -f /root/devops_task3/deploy.yml")
 for root,dirs,files in os.walk('/root/devops_task3'):
     print(files)
     for file in files:
         if file.endswith('.php'):
-          os.system("kubectl create -f jenkins_pvc.yml")
+          os.system("kubectl create -f /root/devops_task3/deploy.yml")
           os.system("kubectl expose deploy myweb-deploy --type=NodePort --port=80")
         elif file.endswith('.html'):
           os.system("kubectl create -f /root/devops_talk3/deploy1.yml")
