e2018e4a8804017b55a10f6ef3aa2928a740436e
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-27 16:52:41 +0530

    For checking the status of code

---
+++ checkstatus.py
@@ -0,0 +1,9 @@
+import susprocess as sp:
+    if(sp.getoutput("curl -o /dev/null -s -w \"%{http_code}\n\" php_code")==                 "200"): 
+        pass
+    elif(sp.getoutput("curl -o /dev/null -s -w \"%{http_code}\n\" html_code")==            "200"):
+        pass
+    else:
+        with open("/status" , "w") as f:
+            f.write("SEND EMAIL")
+
