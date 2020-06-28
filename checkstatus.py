1ae29dcc703f8181ca1f8075c00fdd4f5c369aef
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-28 16:04:10 +0530

    Delete checkstatus.py

--- checkstatus.py
+++
@@ -1,9 +0,0 @@
-import os
-if(os.system("$(curl -o /dev/null -s -w \"%{http_code}\" 192.168.99.108:31000/*.php)") == "200"): 
-    pass
-elif(os.system("$(curl -o /dev/null -s -w \"%{http_code}\" 192.168.99.108:31000/*.html)") == "200"):
-    pass
-else:
-    with open("/status" , "w") as f:
-         f.write("Code is not Correct")
-
