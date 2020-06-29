534ad728d7cf620799e0aa650fd8d987bd324340
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-29 19:20:23 +0530

    Get the extension of code and launch deployment

--- extension.py
+++ extension.py
@@ -3,7 +3,7 @@ for root,dirs,files in os.walk('/root/devops_task3'):
     print(files)
     for file in files:
         if file.endswith('.html'):
-          os.system("kubectl create -f /root/devops_task3/service.yml")
-          os.system("kubectl create -f /root/devops_task3/jenkins_pvc.yml")
-          os.system("kubectl create -f /root/devops_task3/deploy.yml")
+           os.system("kubectl create -f /root/devops_task3/service.yml")
+           os.system("kubectl create -f /root/devops_task3/jenkins_pvc.yml")
+           os.system("kubectl create -f /root/devops_task3/deploy.yml")
     
