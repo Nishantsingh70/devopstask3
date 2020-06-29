9c6384b9b3fa5238e6e4ac588ad353ec1bc3c559
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-29 19:22:40 +0530

    Code for sending the mail

--- sendemail.py
+++ sendemail.py
@@ -1,7 +1,7 @@
 import smtplib
 sender_email = "nishantsingh70600@gmail.com"
 receiver_email = "nishantsingh9527@gmail.com"
-password = "********"
+password = "*******"
 message = "Your code is working good"
 server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
