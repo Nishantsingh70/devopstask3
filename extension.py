33521076476731e68f1b0cfd21b519d0c6b7f322
Nishantsingh70 <64470189+Nishantsingh70@users.noreply.github.com>
2020-06-29 16:18:06 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -2,7 +2,7 @@ import os
 for root,dirs,files in os.walk('/root/devops_task3'):
     print(files)
     for file in files:
-        if file.endswith('.php'):
+        if file.endswith('.html'):
           os.system("kubectl create -f /root/devops_task3/service.yml")
           os.system("kubectl create -f /root/devops_task3/jenkins_pvc.yml")
           os.system("kubectl create -f /root/devops_task3/deploy.yml")
